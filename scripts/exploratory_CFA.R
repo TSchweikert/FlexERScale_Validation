942e369db5e7e8a3f6e9e137a2f220a6145fe842
Christoph Scheffel <christoph_scheffel@tu-dresden.de>
2026-03-20 15:12:58 +0100

    power

--- scripts/exploratory_CFA.R
+++ scripts/exploratory_CFA.R
@@ -64,6 +64,7 @@ data_CFA$ERF_mean <- rowMeans(data_CFA[c("flexer_01", "flexer_02", "flexer_03", 
 
 data_CFA$ERF <- 5 - data_CFA$ERF_mean
 
+
 ### CONFIRMATORY FACTORY ANALYSES
 
 # model with one factor
@@ -86,8 +87,45 @@ summary(model_2factor, sta=T,fit.measure =TRUE)
 
 # model comparison
 
-# Modellvergleich (Standard-Schätzer)
+# 
 anova(model_1factor, model_2factor)
-# oder
+
+# or
 lavTestLRT(model_1factor, model_2factor)
 
+# Chi-Squared Difference Test
+
+#               Df    AIC    BIC   Chisq Chisq diff  RMSEA Df diff
+# model_2factor 34 3350.1 3414.9  68.668                          
+# model_1factor 35 3452.4 3514.2 172.987     104.32 0.7986       1
+# Pr(>Chisq)    
+# model_2factor               
+# model_1factor  < 2.2e-16 ***
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+### POWER ANALYSIS
+
+# Poweranalyse durchführen
+power <- semPower::semPower.postHoc(0.079,
+                          "RMSEA",
+                          alpha = .05,
+                          N = 162,
+                          df = 34)
+summary(power)
+
+# semPower: Post hoc power analysis
+
+# F0                       0.212194
+# RMSEA                    0.079000
+# Mc                       0.899337
+
+# df                       34      
+# Num Observations         162     
+# NCP                      34.16323
+
+# Critical Chi-Square      48.60236
+# Alpha                    0.050000
+# Beta                     0.074870
+# Power (1 - Beta)         0.925130
+# Implied Alpha/Beta Ratio 0.667825
