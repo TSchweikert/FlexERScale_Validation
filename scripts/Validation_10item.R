dc93d283c47752e7cff7be94dfe008bca7e224a4
Christoph Scheffel <christoph_scheffel@tu-dresden.de>
2026-04-07 15:44:13 +0200

    Update Validation_10item.R

--- scripts/Validation_10item.R
+++ scripts/Validation_10item.R
@@ -12,7 +12,7 @@
 library(here)             # find path to Rproj
 here::i_am("FlexERScale.Rproj")
 library(renv)             # for computational reproducibility
-
+library(sjPlot)
 library(dplyr)          
 library(foreign)
 library(psych)
@@ -24,6 +24,9 @@ library(reshape2)
 library(MVN)
 library(GPArotation)
 library(nFactors)
+library(lavaan)    # CFA/SEM
+library(semTools)  # Additional functions for reliability, validity, etc.
+library(semPlot)
 
 ### DATA IMPORT
 
@@ -462,6 +465,182 @@ ggplot(data = df_complete, mapping = aes(x = ERF, y = WHO)) +
          label = bquote(paste(rho) == .(round(cor_ERF_WHO, 3))), 
          size = 7, color = "black",
          hjust = 0)  
+
+############ CONFIRMATORY POWER ANALYSIS
+
+### DATA IMPORT
+
+# import questionnaire data from ICER-SV Study
+
+data_icersv <- read.csv(here("datasheets", "10item", "ICER_SV_Survey.csv"),
+                        stringsAsFactors = FALSE, header = TRUE,
+                        na.strings = c("", "NA"))
+colnames(data_icersv)[1] <- "set" # rename the first column
+# tidy up data frame:
+# fill subject id in each row and collapse arms into one row per participant
+
+data_icersv <- data_icersv %>% 
+  data.table::as.data.table() %>% 
+  replace(is.na(data_icersv), "")
+
+data_icersv <- data_icersv[, lapply(.SD, paste0 , collapse=""), by=set]
+
+data_icersv <- as.data.frame(data_icersv)
+
+data_icersv <- data_icersv %>%
+  subset(!is.na(set), select = c(set, age, gender, edu,  grep("flexer", colnames(data_icersv))))
+
+data_icersv[, c("age", "gender", "edu")] <- sapply(data_icersv[, c("age", "gender", "edu")], as.numeric)
+
+#### Flexible Emotion Regulation Scale - FlexER
+
+# store flexer items in separate df
+# only keep rows with complete flexer questionnaire
+data_CFA <- data_icersv %>% 
+  subset(select = c(set,
+                    grep("flexer", colnames(data_icersv)))) %>% 
+  dplyr::filter((flexer_scale_complete == 2))
+
+data_CFA[, c("flexer_01", "flexer_02", "flexer_03", "flexer_04", "flexer_05", "flexer_06", "flexer_07", "flexer_08", "flexer_09", "flexer_10")] <- sapply(data_CFA[, c("flexer_01", "flexer_02", "flexer_03", "flexer_04", "flexer_05", "flexer_06", "flexer_07", "flexer_08", "flexer_09", "flexer_10")], as.numeric)
+
+# compute ERF mean score --> lower values reflect higher flexibility
+data_CFA$ERF_mean <- rowMeans(data_CFA[c("flexer_01", "flexer_02", "flexer_03", "flexer_04", "flexer_05", "flexer_06", "flexer_07", "flexer_08", "flexer_09", "flexer_10")])
+
+# recode mean score, because we want higher values to reflect higher flexibility
+
+data_CFA$ERF <- 5 - data_CFA$ERF_mean
+
+##### OMEGA
+
+# Für ERF1-Items
+
+items_erf1 <-
+  c("flexer_01",
+    "flexer_04",
+    "flexer_05",
+    "flexer_06",
+    "flexer_07",
+    "flexer_10")
+
+omega_erf1 <- omega(data_CFA[, items_erf1],
+                    plot 
+                    = FALSE)
+
+print(omega_erf1$omega.tot)
+
+# 0.8505307
+
+# Für ERF2-Items
+
+items_erf2 <-
+  c("flexer_02",
+    "flexer_03",
+    "flexer_08",
+    "flexer_09")
+
+omega_erf2 <- omega(data_CFA[, items_erf2],
+                    plot 
+                    = FALSE)
+
+print(omega_erf2$omega.tot)
+
+# 0.8505307
+
+
+# Für Gesamtskala (10 Items)
+
+totalerf <-
+  c("flexer_01",
+    "flexer_04",
+    "flexer_05",
+    "flexer_06",
+    "flexer_07",
+    "flexer_10",
+    "flexer_02",
+    "flexer_03",
+    "flexer_08",
+    "flexer_09")
+
+omega_total <- omega(data_CFA[, totalerf],
+                     plot 
+                     = FALSE)
+
+print(omega_total$omega.tot)
+
+# 0.7615312
+
+### CONFIRMATORY FACTORY ANALYSES
+
+item_names <- c("flexer_01", "flexer_02", "flexer_03", "flexer_04", "flexer_05",
+                "flexer_06", "flexer_07", "flexer_08", "flexer_09", "flexer_10")
+data_CFA[item_names] <- lapply(data_CFA[item_names], ordered)
+
+# model with one factor
+
+model_1factor <- '
+  ERF1 =~ flexer_01 + flexer_02 + flexer_03 + flexer_04 + flexer_05 + flexer_06 + flexer_07 + flexer_08 + flexer_09 + flexer_10 
+'
+model_1factor <- cfa(model_1factor, data = data_CFA, estimator ="WLSMV", ordered = item_names)
+semPlot::semPaths(model_1factor, whatLabels="stand")
+summary(model_1factor, sta=TRUE, fit.measure =TRUE)
+
+lavInspect(model_1factor, "est")$lambda
+lavInspect(model_1factor, "std.lv")$lambda
+# model with two factors
+
+model_2factor <- '
+  ERF1 =~ flexer_01 + flexer_04 + flexer_05 + flexer_06 + flexer_07 + flexer_10
+  ERF2 =~ flexer_02 + flexer_03 + flexer_08 + flexer_09 
+'
+
+model_2factor <- cfa(model_2factor, data = data_CFA, estimator ="WLSMV", ordered = item_names)
+semPlot::semPaths(model_2factor, whatLabels="stand")
+summary(model_2factor, sta=T,fit.measure =TRUE)
+
+# model comparison
+
+lavTestLRT(model_1factor, model_2factor)
+
+# Scaled Chi-Squared Difference Test (method = “satorra.2000”)
+
+# lavaan->lavTestLRT():  
+#   lavaan NOTE: The “Chisq” column contains standard test 
+# statistics, not the robust test that should be reported per 
+# model. A robust difference test is a function of two standard 
+# (not robust) statistics.
+#               Df AIC BIC   Chisq Chisq diff Df diff Pr(>Chisq)    
+# model_2factor 34          60.367                                  
+# model_1factor 35         307.942     59.722       1  1.093e-14 ***
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+### POWER ANALYSIS
+
+# Poweranalyse durchführen
+power <- semPower::semPower.postHoc(0.086, # RMSEA
+                                    "RMSEA",
+                                    alpha = .05,
+                                    N = 162,
+                                    df = 34)
+summary(power)
+
+# semPower: Post hoc power analysis
+
+# F0                       0.251464
+# RMSEA                    0.086000
+# Mc                       0.881851
+
+# df                       34      
+# Num Observations         162     
+# NCP                      40.48570
+
+# Critical Chi-Square      48.60236
+# Alpha                    0.050000
+# Beta                     0.031481
+# Power (1 - Beta)         0.968519
+# Implied Alpha/Beta Ratio 1.588268
+
+
 ############
 
 # save workspace image
